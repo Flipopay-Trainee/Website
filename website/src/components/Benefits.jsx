ccc7c5fb22e04ebb4d57c0b646c6a82d5b8e1c5d
Prakash <prakashmadhavan28@gmail.com>
2024-05-15 19:30:11 +0530

    completed

---
+++ website/src/components/Benefits.jsx
@@ -0,0 +1,59 @@
+import Topline from '..//assets/Topline.png'
+import Bottomline from '..//assets/Bottomline.png'
+
+
+function Benefits(){
+    return(
+        <div className="benefit-container">
+            <div className="ben-head">
+                <h3 className='benefit-head1'>Road  to</h3>
+                <h2 className='ben-head2'>FUTURE</h2>
+                <hr className="line2"></hr>
+            </div>
+            <div className="benefit">
+                <div className="box1">
+                    <img className='ben-img1' src={Topline}/>
+                    <h2 className='ben-head1'>01</h2>
+                    <p className='ben-para1'>Membership status</p>
+                </div>
+                <div className='box2'>
+                    <h2 className='ben-head1'>02</h2>
+                    <p className='ben-para1'>Business platform & Networking</p>
+                    <img className='ben-img1' src={Bottomline}/>
+                </div>
+                <div className="box3">
+                    <img className='ben-img1' src={Topline}/>
+                    <h2 className='ben-head1'>03</h2>
+                    <p className='ben-para1'>Enjoy privilages & Rewards</p>
+                </div>
+                <div className='box4'>
+                    <h2 className='ben-head1'>04</h2>
+                    <p className='ben-para1'>Enjoy Royalty & Bonus</p>
+                    <img className='ben-img1' src={Bottomline}/>
+                </div>
+                <div className="box5">
+                    <img className='ben-img1' src={Topline}/>
+                    <h2 className='ben-head1'>05</h2>
+                    <p className='ben-para1'>Increased Membership Value every year</p>
+                </div>
+                <div className='box6'>
+                    <h2 className='ben-head1'>06</h2>
+                    <p className='ben-para1'>6-20% Interest will be paid continuously for 10 year</p>
+                    <img className='ben-img1' src={Bottomline}/>
+                </div>
+                <div className="box7">
+                    <img className='ben-img1' src={Topline}/>
+                    <h2 className='ben-head1'>07</h2>
+                    <p className='ben-para1'>Business coaching & Training</p>
+                </div>
+            
+            </div>
+
+        </div>
+    )
+}
+
+
+
+
+export default Benefits
---
+++ website/src/components/Network.jsx
@@ -0,0 +1,25 @@
+
+
+
+function Network(){
+
+return(
+    <div className="network-container">
+        <div className="network">
+            <h3 className="net-head1">Join</h3>
+            <h2 className="net-head2">KEY GLOBAL NETWORK</h2>
+            <button className="net-btn">GET YOUR KEY TO FUTURE</button>
+        </div>
+    </div>
+)
+
+
+}
+
+
+
+
+
+
+
+export default Network
